c302a399a8f2d21ebc469e55fe487bad2f68302f
Eva Gonalez Ferreira <evagonf12@gmail.com>
2021-05-29 09:45:37 +0200

    Start News Challenge

---
+++ NewsChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsChallenge
+//
+//  Created by Eva Gonzalez Ferreira on 29/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
